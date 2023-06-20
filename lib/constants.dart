ff11c6dc6bf6253124d374e718817d3dbd9cd71d
Jov247 <jovinjoyark@gmail.com>
2023-06-20 20:00:40 +0530

    Final version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // ignore: constant_identifier_names
-const String google_api_key = "APIKEY";
+const String google_api_key = "AIzaSyAd0tYnbFms4NZhiRBIUr3tBqdBokE5LJk";
 const Color primaryColor = Color(0xFF7B61FF);
 const double defaultPadding = 16.0;
 
